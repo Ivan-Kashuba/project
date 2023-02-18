37cedabfb50eaf7fae95c2da69895089c4a638f5
Ivan Kashuba <kashubaivan7@gmail.com>
2023-02-18 17:41:50 +0200

    Add loki screens

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,5 +1,5 @@
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import { render, screen } from '@testing-library/react';
+import { Button, ThemeButton } from './Button';
 
 describe('Button', () => {
     test('Appear on the page', () => {
@@ -9,6 +9,5 @@ describe('Button', () => {
     test('Have button theme class', () => {
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
-        screen.debug();
     });
 });
