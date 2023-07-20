e2dedaf96015a303937e6a3b66984b6cc72062f2
Ivan Kashuba <kashubaivan7@gmail.com>
2023-07-20 12:18:29 +0300

    Split build and ts checks in webpack

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -9,17 +9,17 @@ export interface BuildPaths {
     buildLocales: string;
 }
 
+export interface BuildEnv {
+    mode: BuildMode;
+    port: number;
+    apiUrl: string;
+}
+
 export interface BuildOptions {
     mode: BuildMode;
     paths: BuildPaths;
     isDev: boolean;
     port: number;
     apiUrl: string;
-    project: 'storybook' | 'frontend' | 'jest'
-}
-
-export interface BuildEnv {
-    mode: BuildMode;
-    port: number;
-    apiUrl: string;
+    project: 'storybook' | 'frontend' | 'jest';
 }
